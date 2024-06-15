7edb9f0806ae136f2ba49e4c962c237e47141180
Derek <56985040+derekmurphy1993@users.noreply.github.com>
2024-06-15 12:39:08 -0500

    [feat] Added basic page files, installed tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,3 @@
-import React from 'react'
-
 export default function App() {
-  return (
-    <div>App</div>
-  )
+	return <div className="font-bold text-green-500">App name</div>;
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,tsx,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
