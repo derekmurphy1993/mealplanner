14ddaf159b857168757e384e6de062a623b1bd77
Derek <56985040+derekmurphy1993@users.noreply.github.com>
2024-06-17 23:33:36 -0500

    [feat] added routes to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,20 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Planner from "./pages/Planner";
+import Profile from "./pages/Profile";
+
 export default function App() {
-	return <div className="font-bold text-green-500">App name</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/sign-up" element={<SignUp />} />
+				<Route path="/plan" element={<Planner />} />
+				<Route path="/profile" element={<Profile />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
