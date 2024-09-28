07d98ceb4817db5caff0c10b664c49ee6ba8539d
Derek <derekmurphy1993@gmail.com>
2024-09-28 16:06:24 -0500

    [feat] Get Meal Route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,48 +1,62 @@
 import User from "../models/user.model.js";
+import Meal from "../models/meal.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 
 export const test = (req, res) => {
-	res.json({
-		message: "hello",
-	});
+  res.json({
+    message: "hello",
+  });
 };
 
 export const updateUser = async (req, res, next) => {
-	if (req.user.id !== req.params.id)
-		return next(errorHandler(401, "You cannot update this account"));
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You cannot update this account"));
 
-	try {
-		if (req.body.password) {
-			req.body.password = bcryptjs.hashSync(req.body.password, 10);
-		}
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
 
-		const updatedUser = await User.findByIdAndUpdate(
-			req.params.id,
-			// do not just "$set" leaves oppertunity to change other info
-			{
-				$set: {
-					username: req.body.username,
-					email: req.body.password,
-					password: req.body.password,
-					avatar: req.body.avatar,
-				},
-			},
-			{ new: true }
-		);
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      // do not just "$set" leaves oppertunity to change other info
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.password,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
 
-		const { password, ...rest } = updatedUser._doc;
-		res.status(200).json(rest);
-	} catch (error) {}
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {}
 };
 
 export const deleteUser = async (req, res, next) => {
-	if (req.user.id !== req.params.id)
-		return next(errorHandler(401, "You can only delete your own account!"));
-	try {
-		await User.findByIdAndDelete(req.params.id);
-		res.status(200).json("user has been deleted");
-	} catch (error) {
-		next(error);
-	}
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only delete your own account!"));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("user has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserMeals = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const meals = await Meal.find({ userRef: req.params.id });
+      res.status(200).json(meals);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own meals!"));
+  }
 };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import {
-	test,
-	updateUser,
-	deleteUser,
+  test,
+  updateUser,
+  deleteUser,
+  getUserMeals,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/meals/:id", verifyToken, getUserMeals);
 
 export default router;
