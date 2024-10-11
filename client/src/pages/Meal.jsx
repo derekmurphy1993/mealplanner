60d22edc46d0e031a10fdb07f0ad99b5c065be9a
Derek <derekmurphy1993@gmail.com>
2024-10-11 13:47:19 -0500

    [style] Meal styling

--- client/src/pages/Meal.jsx
+++ client/src/pages/Meal.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 export default function Meal() {
   const [meal, getMeal] = useState(null);
   const params = useParams();
+
   useEffect(() => {
     const fetchMeal = async () => {
       const mealId = params.mealId;
@@ -20,14 +21,29 @@ export default function Meal() {
   });
 
   return (
-    <div className="flex">
-      <div className="flex flex-row">
-        <h1 className="font-bold text-xl text-blue-950">{meal.name}</h1>
-        <button className="bg-green-400 rounded-lg p-3 font-semibold uppercase text-slate-100">
-          {" "}
-          Update{" "}
-        </button>
-      </div>
+    <div>
+      {meal && (
+        <div className="flex-col min-w-full m-h-full justify-items-center items-center">
+          <div className="flex flex-row min-w-screen justify-center items-center">
+            <h1 className="font-bold text-xl text-blue-950 text-center">
+              {meal.name || "error"}
+            </h1>
+            <button className="bg-green-400 rounded-lg p-3 font-semibold uppercase text-slate-100">
+              {" "}
+              Update{" "}
+            </button>
+          </div>
+          <div className="flex-row">
+            <div className="border w-4/12">IMAGE</div>
+            <div className="border flex-1 flex-col w-8/12">
+              <p className="font-semibold">Calories: {meal.calories}</p>
+              <p>Protien: {meal.prots}</p>
+              <p>Fats: {meal.fats}</p>
+              <p>Carbs: {meal.carbs}</p>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
