1ef83f351dda46959fff53eaafd7e35eccb59d2a
Derek <derekmurphy1993@gmail.com>
2025-11-11 12:17:45 -0600

    Style Tweak MealCard Component

--- client/src/components/MealCard.jsx
+++ client/src/components/MealCard.jsx
@@ -9,42 +9,46 @@ export default function MealCard({ meal }) {
   // ICON for liking, icon for if it has a link to recipie, icon for if it has recipie built in :)
 
   return (
-    <div className="relative max-w-52 min-w-52 w-full lg:max-w-md xl:max-w-lg h-72 bg-white overflow-hidden rounded-md shadow-xl border-2 border-gray-700 border-opacity-35 group">
+    <div className="relative max-w-52 min-w-52 w-full lg:max-w-56 xl:max-w-lg xl:gap-14 h-72 bg-white overflow-hidden rounded-md shadow-xl border-2 border-gray-700 border-opacity-35 group hover:scale-105 hover:drop-shadow-2xl hover:z-30">
       <Link to={`/meal/${meal._id}`}>
-        <div className="overflow-hidden items-center h-[40%]  flex justify-center  border-frenchblue-500">
+        <div className="overflow-hidden items-center h-[40%]  flex justify-center ">
           {meal.image ? (
             <img
               src={meal.image}
               alt={"Image of " + meal.name}
-              className=" transition-transform group-hover:scale-110 duration-200 object-cover"
+              className=" transition-transform duration-200 object-cover"
             />
           ) : (
             <img
               src={placeholderimg}
               alt={"Placeholder image, no image found for " + meal.name}
-              className="transition-transform group-hover:scale-110 duration-200  border-2"
+              className="transition-transform duration-200  border-2"
             />
           )}
         </div>
         <div className="p-2 text-azul-800">
-          <p className="font-semibold mx-2 text-lg">{meal.name}</p>
+          <p className="font-semibold mx-2 text-lg xl:text-2xl xl:ml-10 border-t-2 border-frenchblue-200 border-opacity-45">
+            {meal.name}
+          </p>
         </div>
-        <p className="font-semibold p-1 mx-6">Calories: {meal.calories}</p>
-        <div className="flex justify-between mx-8 flex-wrap">
-          <div className="mt-0 text-sm flex flex-col justify-center items-center">
-            <p className="border-b-2 border-b-slate-300">Protein</p>
-            <p className="font-semibold">{meal.prots}g</p>
+        <div className="max-w-96 mx-auto ">
+          <p className="font-semibold p-1 mx-6">Calories: {meal.calories}</p>
+          <div className="flex justify-between mx-8 flex-wrap">
+            <div className="mt-0 text-sm flex flex-col justify-center items-center">
+              <p className="border-b-2 border-b-slate-300">Protein</p>
+              <p className="font-semibold">{meal.prots}g</p>
+            </div>
+            <div className="mt-0 text-sm flex flex-col justify-center items-center">
+              <p className="border-b-2 border-b-slate-300">Fats</p>
+              <p className="font-semibold">{meal.fats}g</p>
+            </div>
+            <div className="mt-0 text-sm flex flex-col justify-center items-center">
+              <p className="border-b-2 border-b-slate-300">Carbs</p>
+              <p className="font-semibold">{meal.carbs}g</p>
+            </div>{" "}
           </div>
-          <div className="mt-0 text-sm flex flex-col justify-center items-center">
-            <p className="border-b-2 border-b-slate-300">Fats</p>
-            <p className="font-semibold">{meal.fats}g</p>
-          </div>
-          <div className="mt-0 text-sm flex flex-col justify-center items-center">
-            <p className="border-b-2 border-b-slate-300">Carbs</p>
-            <p className="font-semibold">{meal.carbs}g</p>
-          </div>{" "}
+          {/* <FaHeart /> <FaRegHeart /> */}
         </div>
-        {/* <FaHeart /> <FaRegHeart /> */}
       </Link>
     </div>
   );
