0f349e3675b6b3ad5ba7c823d1f121d946704226
Derek <derekmurphy1993@gmail.com>
2026-03-08 17:49:05 -0500

    bypass popup

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,4 +1,10 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { useEffect } from "react";
+import {
+	GoogleAuthProvider,
+	getAuth,
+	getRedirectResult,
+	signInWithRedirect,
+} from "firebase/auth";
 import { app } from "../firebase";
 import { useDispatch } from "react-redux";
 import { signInSuccess } from "../redux/user/userSlice";
@@ -8,27 +14,42 @@ import { apiFetch } from "../utils/api";
 export default function OAuth() {
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
+
+	useEffect(() => {
+		const handleGoogleRedirect = async () => {
+			try {
+				const auth = getAuth(app);
+				const result = await getRedirectResult(auth);
+
+				if (!result?.user) return;
+
+				const res = await apiFetch("/api/auth/google", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						name: result.user.displayName,
+						email: result.user.email,
+						photo: result.user.photoURL,
+					}),
+				});
+				const data = await res.json();
+				dispatch(signInSuccess(data));
+				navigate("/");
+			} catch (err) {
+				console.log("Could not complete Google redirect sign in", err);
+			}
+		};
+
+		handleGoogleRedirect();
+	}, [dispatch, navigate]);
+
 	const handleGoogle = async () => {
 		try {
 			const provider = new GoogleAuthProvider();
 			const auth = getAuth(app);
-
-			const result = await signInWithPopup(auth, provider);
-
-			const res = await apiFetch("/api/auth/google", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					name: result.user.displayName,
-					email: result.user.email,
-					photo: result.user.photoURL,
-				}),
-			});
-			const data = await res.json();
-			dispatch(signInSuccess(data));
-			navigate("/");
+			await signInWithRedirect(auth, provider);
 		} catch (err) {
 			console.log("Could not sign in with google", err);
 		}
